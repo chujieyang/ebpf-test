168f9ceee95ae595bc4490113846a2f27e8a8231
yangchujie <yangchujie1@163.com>
2023-09-12 15:43:20 +0800

    init
    Signed-off-by: yangchujie <yangchujie1@163.com>

---
+++ bpfgo/main.go
@@ -0,0 +1,58 @@
+/*
+ * @Author: yangchujie yangchujie1@163.com
+ * @Date: 2023-09-12 14:33:31
+ * @LastEditors: yangchujie yangchujie1@163.com
+ * @LastEditTime: 2023-09-12 14:33:37
+ * @FilePath: /ebpf-test/bpfgo/main.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package main
+
+import "C"
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"os"
+
+	bpf "github.com/aquasecurity/tracee/libbpfgo"
+)
+
+func main() {
+
+	bpfModule, err := bpf.NewModuleFromFile("simple.bpf.o")
+	if err != nil {
+		os.Exit(-1)
+	}
+	defer bpfModule.Close()
+
+	bpfModule.BPFLoadObject()
+	prog, err := bpfModule.GetProgram("kprobe__sys_execve")
+	if err != nil {
+		os.Exit(-1)
+	}
+
+	_, err = prog.AttachKprobe("__x64_sys_execve")
+	if err != nil {
+		os.Exit(-1)
+	}
+
+	eventsChannel := make(chan []byte)
+	rb, err := bpfModule.InitRingBuf("events", eventsChannel)
+	if err != nil {
+		os.Exit(-1)
+	}
+
+	rb.Start()
+
+	for {
+		event := <-eventsChannel
+		pid := int(binary.LittleEndian.Uint32(event[0:4])) // Treat first 4 bytes as LittleEndian Uint32
+		comm := string(bytes.TrimRight(event[4:], "\x00")) // Remove excess 0's from comm, treat as string
+		fmt.Printf("%d %v\n", pid, comm)
+	}
+
+	rb.Stop()
+	rb.Close()
+}
